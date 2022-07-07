9e8f0f00bd3c92f98146f6328fe1c906b09c4098
yoshi1203 <yoshivn1203@gmail.com>
2022-07-08 01:41:48 +0700

    finish

---
+++ js/main.js
@@ -0,0 +1,39 @@
+timMin = () => {
+  let sum = 0,
+    n = 0;
+  while (sum < 1e4) {
+    n++;
+    sum += n;
+  }
+  document.getElementById('ketQua1').innerHTML = n;
+};
+
+tinhTong = () => {
+  var x = document.getElementById('inputX').value,
+    n = document.getElementById('inputN').value;
+  for (sum = 0, i = 1; i <= n; i++) {
+    sum += Math.pow(x, i);
+  }
+  document.getElementById('ketQua2').innerHTML = sum;
+};
+
+tinhGiaiThua = () => {
+  var n = document.getElementById('inputN2').value;
+  for (var result = 1, i = 1; i <= n; i++) {
+    result *= i;
+  }
+  document.getElementById('ketQua3').innerHTML = result;
+};
+
+taoTheDiv = () => {
+  let div = '',
+    sumDiv = '';
+  for (i = 1; i <= 10; i++) {
+    div =
+      i % 2 == 0
+        ? "<div class='bg-danger text-white p-2'>Div chẵn</div>"
+        : "<div class='bg-primary text-white  p-2'>Div lẻ </div>";
+    sumDiv += div;
+  }
+  document.getElementById('ketQua4').innerHTML = sumDiv;
+};
